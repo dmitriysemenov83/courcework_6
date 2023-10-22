5e960b6d1892f1869136848ba4515d4b354a8b5f
Dmitriy <xs_ds@inbox.ru>
2023-10-22 22:59:58 +0500

    completion stage доработка 6

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from users.models import User
 
+admin.site.register(User)
 
-@admin.register(User)
-class AdminAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'phone', 'country', 'avatar')
+
+# @admin.register(User)
+# class AdminAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'username', 'email', 'phone', 'country', 'avatar')
